12f44f27ba6ee5327e9410152f0553e98acd0e5d
George Spanos <gespa11019@gmail.com>
2026-01-11 17:10:32 +0200

    refactor: add deno testing lib

--- src/core/src/board/board.test.ts
+++ src/core/src/board/board.test.ts
@@ -1,16 +1,16 @@
-import { assert } from 'https://deno.land/std@0.217.0/assert/assert.ts';
-import { validateBoard } from './board.ts';
+import { assert } from "@std/assert";
+import { validateBoard } from "./board.ts";
 
 Deno.test("should test invalid boards", () => {
-  assert(validateBoard(['Bc', 2, false]) === false);
+  assert(validateBoard(["Bc", 2, false]) === false);
   assert(validateBoard({}) === false);
-  assert(validateBoard(false) === false); ``;
-  assert(validateBoard(['Qc', 'Qs', 'Td', 'As', '3d', '2s']) === false);
+  assert(validateBoard(false) === false);
+  ``;
+  assert(validateBoard(["Qc", "Qs", "Td", "As", "3d", "2s"]) === false);
 });
-Deno.test('should test valid boards', ()=> {
+Deno.test("should test valid boards", () => {
   assert(validateBoard([]));
-  assert(validateBoard(['Ac', '3s', 'Th']));
-  assert(validateBoard(['Qc', 'Qs', 'Td', 'As']));
-  assert(validateBoard(['Qc', 'Qs', 'Td', 'As', '3d']));
-})
-
+  assert(validateBoard(["Ac", "3s", "Th"]));
+  assert(validateBoard(["Qc", "Qs", "Td", "As"]));
+  assert(validateBoard(["Qc", "Qs", "Td", "As", "3d"]));
+});
